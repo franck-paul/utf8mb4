212b67110ec2537089d7b98a0a2ac5c4ab100304
franck <carnet.franck.paul@gmail.com>
2024-12-14 15:43:18 +0100

    JS code review (WIP)

--- js/post.js
+++ js/post.js
@@ -1,23 +1,21 @@
 /*global $, dotclear, he */
 'use strict';
 
-$(() => {
+dotclear.ready(() => {
   Object.assign(dotclear, dotclear.getData('utf8mb4'));
 
-  $('#edit-entry').on('onetabload', () => {
-    if (dotclear.utf8mb4n_notes_only === 0) {
-      const excerpt = $('#post_excerpt').val();
-      const content = $('#post_content').val();
-      if (excerpt !== undefined) {
-        $('#post_excerpt').val(he.decode(excerpt));
-      }
-      if (content !== undefined) {
-        $('#post_content').val(he.decode(content));
-      }
+  if (dotclear.utf8mb4n_notes_only === 0) {
+    const excerpt = $('#post_excerpt').val();
+    const content = $('#post_content').val();
+    if (excerpt !== undefined) {
+      $('#post_excerpt').val(he.decode(excerpt));
     }
-    const notes = $('#post_notes').val();
-    if (notes !== undefined) {
-      $('#post_notes').val(he.decode(notes));
+    if (content !== undefined) {
+      $('#post_content').val(he.decode(content));
     }
-  });
+  }
+  const notes = $('#post_notes').val();
+  if (notes !== undefined) {
+    $('#post_notes').val(he.decode(notes));
+  }
 });
